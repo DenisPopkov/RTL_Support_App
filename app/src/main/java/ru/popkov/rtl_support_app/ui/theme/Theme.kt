5ed522f23c739091e8bbc52dfd6a6f02b7ec9a54
Denis Popkov <popkov-den03@mail.ru>
2024-02-03 14:06:48 +0600

    fix: ui issues

--- app/src/main/java/ru/popkov/rtl_support_app/ui/theme/Theme.kt
+++ app/src/main/java/ru/popkov/rtl_support_app/ui/theme/Theme.kt
@@ -59,8 +59,8 @@ fun RTLSupportAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
-            window.navigationBarColor = colorScheme.background.toArgb()
+            window.statusBarColor = colorScheme.surface.toArgb()
+            window.navigationBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
